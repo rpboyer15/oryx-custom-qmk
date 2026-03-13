caea682a1e0fd3babae1408c025dc571a6f0dc5b
Reed Boyer <rpboyer15@gmail.com>
2026-03-12 17:43:01 -0700

    fix custom file paths

--- patch_custom.sh
+++ patch_custom.sh
@@ -12,7 +12,7 @@ LAYOUT_DIR="${1:?Usage: $0 <layout_dir>}"
 echo "Patching ${LAYOUT_DIR}/keymap.c ..."
 
 # 1. Add custom include after the QMK_KEYBOARD_H include
-sed -i 's|^#include QMK_KEYBOARD_H|#include QMK_KEYBOARD_H\n#include "custom_keymap.h"|' \
+sed -i 's|^#include QMK_KEYBOARD_H|#include QMK_KEYBOARD_H\n#include "features/custom_keymap.h"|' \
   "${LAYOUT_DIR}/keymap.c"
 
 # 2. Insert custom process call at the start of process_record_user body
@@ -23,6 +23,6 @@ sed -i '/^bool process_record_user(uint16_t keycode, keyrecord_t \*record) {$/a\
 echo "Patching ${LAYOUT_DIR}/rules.mk ..."
 
 # 3. Append custom source files to rules.mk
-echo 'SRC += features/custom_shift_keys.c custom_keymap.c' >> "${LAYOUT_DIR}/rules.mk"
+echo 'SRC += features/custom_shift_keys.c features/custom_keymap.c' >> "${LAYOUT_DIR}/rules.mk"
 
 echo "Done. Custom keymap wired in."
