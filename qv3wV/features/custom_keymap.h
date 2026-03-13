4c1a31a0d9313c190943d354555d13a5c7f45045
Reed Boyer <rpboyer15@gmail.com>
2026-03-13 00:09:57 -0700

    CLAUDE CODE - Try disabling Tap Flow for thumb mods only

--- qv3wV/features/custom_keymap.h
+++ qv3wV/features/custom_keymap.h
@@ -4,3 +4,4 @@
 #include "custom_shift_keys.h"
 
 bool process_custom_keymap(uint16_t keycode, keyrecord_t *record);
+uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t flow_tap_term);
